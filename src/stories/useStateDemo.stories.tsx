644cd1d071f202dec74c72a7e1fbe41e97cb0ee3
Valentine <valtikaless@gmail.com>
2021-04-10 09:21:57 +0300

    20-2-useState => customHook => memoization by useMemo\native decision useState takes a function like a parameter , calls her once and works next with return result

--- src/stories/useStateDemo.stories.tsx
+++ src/stories/useStateDemo.stories.tsx
@@ -3,13 +3,22 @@ import {useState} from "react";
 export default {
     title:'useStateDemo'
 }
+function generateData () {
+    //....
+    console.log('generate data')
+    return 202020202020
+}
 export const Example1 = () => {
     console.log('example1')
-    const [counter,setCounter] = useState(0)
+
+
+
+    const [counter,setCounter] = useState(generateData) // [0,function(newValue)()]
 
     return (
         <>
             <button onClick={()=>{setCounter(counter+1)}}></button>
+            {counter}
         </>
     )
 }
