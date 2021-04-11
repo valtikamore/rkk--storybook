80460403733e827cd835eabda1cf632952313af3
Valentine <valtikaless@gmail.com>
2021-04-11 16:40:05 +0300

    22 - use Effect clocks 2 versions

--- src/stories/useEffectDeomo.stories.tsx
+++ src/stories/useEffectDeomo.stories.tsx
@@ -9,6 +9,12 @@ export const SimpleExample = () => {
     useEffect(() => {
         console.log('useEffect')
     })
+    useEffect(() => {
+        console.log('useEffect')
+    }, [])
+    useEffect(() => {
+        console.log('useEffect')
+    }, [counter])
     return (
         <div>
             Hello , {counter}
@@ -16,6 +22,42 @@ export const SimpleExample = () => {
                 setCounter(counter => counter + 1)
             }}> +
             </button>
+        </div>
+    )
+}
+
+
+export const SetTimeoutExample = () => {
+    const [seconds, setSeconds] = useState<number>()
+    const [minuts, setMinuts] = useState<number>()
+    const [hours, setHours] = useState<number>()
+
+    useEffect(() => {
+        setInterval(() => {
+            let date = new Date()
+            setSeconds(date.getUTCSeconds())
+            setMinuts(date.getMinutes())
+            setHours(date.getHours())
+        },1000)
+    }, [])
+    return (
+        <div>
+            <div>{`${hours} ${minuts} ${seconds}`}</div>
+        </div>
+    )
+}
+
+export const Clocks = () => {
+    const [clocks, setClocks] = useState<Date>(new Date())
+
+    useEffect(() => {
+        setInterval(() => {
+            setClocks(new Date())
+        }, 1000)
+    }, [])
+    return (
+        <div>
+            <div>{clocks.toLocaleTimeString()}</div>
         </div>
     )
 }
